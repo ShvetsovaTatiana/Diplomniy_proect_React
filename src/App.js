f2d417a76832e9ecc3e1604230f37a1576a7f6d9
Tatiana Shvetsova <Shvetsovat82@gmail.com>
2025-06-14 23:40:35 +0400

    changed package-lock.json package.json public/index.html src/App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Product from './pages/Product';
+import Registration from './pages/Registration';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/catalog' element={<Catalog />}></Route>
+        <Route path='/product' element={<Product />}></Route>
+        <Route path='/registration' element={<Registration />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
